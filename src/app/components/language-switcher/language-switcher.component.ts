48382cf41327e9a4473b1dc588791dde562e827b
jpalorubio <jpalor03@suarezdefigueroa.es>
2026-04-16 13:49:50 +0200

    estilosmaterial2

--- src/app/components/language-switcher/language-switcher.component.ts
+++ src/app/components/language-switcher/language-switcher.component.ts
@@ -1,23 +1,25 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { LanguageService, AppLanguage } from '../../services/language.service';
 
 @Component({
   selector: 'app-language-switcher',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './language-switcher.component.html',
   styleUrl: './language-switcher.component.scss'
 })
 export class LanguageSwitcherComponent {
-
-
-private languageService = inject(LanguageService);
+  private languageService = inject(LanguageService);
 
   lang = this.languageService.lang;
 
+languages = [
+  { code: 'es' as AppLanguage, flag: 'assets/flags/es.png' },
+  { code: 'en' as AppLanguage, flag: 'assets/flags/gb.png' },
+  { code: 'pt' as AppLanguage, flag: 'assets/flags/pt.png' },
+];
   change(lang: AppLanguage) {
     this.languageService.setLanguage(lang);
   }
-
-
 }
