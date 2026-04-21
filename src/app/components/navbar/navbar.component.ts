3879d975a086047a47073f4e4fe98a6f1314b222
jpalorubio <jpalor03@suarezdefigueroa.es>
2026-04-21 09:27:06 +0200

    responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,28 +1,25 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LanguageSwitcherComponent } from '../language-switcher/language-switcher.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { ThemeSwitcherComponent } from '../theme-switcher/theme-switcher.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule, LanguageSwitcherComponent, TranslateModule, ThemeSwitcherComponent, MatToolbarModule, MatButtonModule],
+  standalone: true,
+  imports: [RouterModule, LanguageSwitcherComponent, TranslateModule, ThemeSwitcherComponent, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
-//array de labels y rutas para el routerlink de los botones
-  menu= [
-
-  { label: 'menu.home', ruta: '/' },
-  { label: 'menu.about', ruta: '/about' },
-  { label: 'menu.projects', ruta: '/projects' },
-  { label: 'menu.contact', ruta: '/contact' },
-
-]
-
+  menu = [
+    { label: 'menu.home', ruta: '/' },
+    { label: 'menu.about', ruta: '/about' },
+    { label: 'menu.projects', ruta: '/projects' },
+    { label: 'menu.contact', ruta: '/contact' },
+  ];
 }
